97261656a45d351acf037c3a98cf55a4f1672cf2
“yakov133” <yakov133@walla.com>
2022-02-14 19:40:34 +0200

    adding axios

--- frontend/src/pages/Movies.jsx
+++ frontend/src/pages/Movies.jsx
@@ -1,6 +1,18 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
 
 const Movies = () => {
+  const [movies, setmovies] = useState([])
+  const URL = ""
+  axios.get(URL)
+  .then(res=>{
+    if(res.status===200){
+      setmovies(res.data);
+    }
+    else{
+      
+    }
+  })
   return (
     <div>Movies</div>
   )
