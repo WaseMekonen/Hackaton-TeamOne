e5889508d0c89ee738a1deee8cdcbd0b3c3c5d11
MekonenWase <mekonen.wase@gmail.com>
2022-02-16 21:45:02 +0200

    add isBusCrowDed function to details page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,20 @@
-import { BrowserRouter, Link, Route, Switch } from 'react-router-dom'
-import Register from './pages/register/Register';
-import Login from './pages/login/Login'
-import Search from './pages/search/Search';
-import Favorites from './pages/favorites/Favorites';
-import Details from './pages/details/Details';
-import './App.css';
+import { BrowserRouter, Link, Route, Switch } from "react-router-dom";
+import Register from "./pages/register/Register";
+import Login from "./pages/login/Login";
+import Search from "./pages/search/Search";
+import Favorites from "./pages/favorites/Favorites";
+import Details from "./pages/details/Details";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <Link to='/Register' >Register</Link>
-        <Link to='/Login' >Login</Link>
-        <Link to='/Search' >Search</Link>
-        <Link to='/Favorites'>Favorites</Link>
-        <Link to='/Details' >Details</Link>
+        <Link to="/Register">Register</Link>
+        <Link to="/Login">Login</Link>
+        <Link to="/Search">Search</Link>
+        <Link to="/Favorites">Favorites</Link>
+        <Link to="/Details">Details</Link>
         <Switch>
           <Route exact path="/Register" render={() => <Register />} />
           <Route exact path="/Login" render={() => <Login />} />
