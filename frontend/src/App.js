49024ac3fad4c4301092c97398d10e1a2a63546b
Uri Zauda <87427105+urizauda@users.noreply.github.com>
2022-02-16 16:04:09 +0200

    finished login and register

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { BrowserRouter, Link, Route, Switch } from 'react-router-dom'
 import Register from './pages/register/Register';
 import Login from './pages/login/Login'
@@ -7,20 +8,23 @@ import Details from './pages/details/Details';
 import './App.css';
 
 function App() {
+  const [auth, setAuth] = useState(null);
+
   return (
     <div className="App">
       <BrowserRouter>
-        <Link to='/Register' >Register</Link>
-        <Link to='/Login' >Login</Link>
-        <Link to='/Search' >Search</Link>
+        <Link to='/Register'>Register</Link>
+        <Link to='/Login'>Login</Link>
+        <Link to='/Search'>Search</Link>
         <Link to='/Favorites'>Favorites</Link>
-        <Link to='/Details' >Details</Link>
+        <Link to='/Details'>Details</Link>
+        <button title='Log out' onClick={() => { setAuth(null); localStorage.clear() }}>Log out</button>
         <Switch>
-          <Route exact path="/Register" render={() => <Register />} />
-          <Route exact path="/Login" render={() => <Login />} />
+          <Route exact path="/Register" render={() => <Register setAuth={setAuth} />} />
+          <Route exact path="/Login" render={() => <Login setAuth={setAuth} />} />
           <Route exact path="/Search" render={() => <Search />} />
-          <Route exact path="/Favorites" render={() => <Favorites />} />
           <Route exact path="/Details" render={() => <Details />} />
+          <Route exact path="/Favorites" render={() => <Favorites />} />
         </Switch>
       </BrowserRouter>
     </div>
--- frontend/src/pages/register/Register.jsx
+++ frontend/src/pages/register/Register.jsx
@@ -1,8 +1,76 @@
-import React from 'react'
-import styles from './register.module.css'
+import React, { useState } from 'react'
+import { Redirect } from 'react-router-dom'
+import axios from 'axios'
+import { SpinnerCircular } from "spinners-react";
+import { API_KEY } from '../../logic/keys';
+import styles from './register.module.css';
 
-export default function Register() {
+export default function Register({ setAuth }) {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [errorFromServer, setErrorFromServer] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [isDisable, setIsDisable] = useState(false);
+  const [redirectToSearch, setRedirectToSearch] = useState(false);
+
+  function register() {
+    const url = `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${API_KEY}`;
+    setLoading(true)
+    axios
+      .post(url, {
+        email,
+        password,
+        confirmPassword
+      })
+      .then(function (response) {
+        setLoading(false)
+        setErrorFromServer(false);
+        setAuth(response.data)
+        console.log(response)
+        setRedirectToSearch(true)
+        localStorage.setItem("userData", JSON.stringify(response.data))
+      })
+      .catch(function (error) {
+        setErrorFromServer(error);
+        setLoading(false)
+        console.log(error);
+      });
+  };
+
+
   return (
-    <div>Register</div>
+    <div>
+      <div>
+        {redirectToSearch ? <Redirect to='/Search' /> : ""}
+        <h1>Sign Up</h1>
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            if (password.length < 6) {
+              alert("password should be at least 6 characters");
+            }
+            else if (password !== confirmPassword) {
+              alert("password must be the same as confirm password");
+            }
+            else {
+              register();
+            }
+          }}
+        >
+          <input type="email" placeholder="E-mail" onChange={(e) => setEmail(e.target.value)} />
+          <br></br>
+          <input type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} />
+          <br></br>
+          <input type="password" placeholder="Confirm Password" onChange={(e) => setConfirmPassword(e.target.value)} />
+          <br></br>
+          <input type="submit" value="Register" disabled={!email || !password || !confirmPassword} />
+        </form>
+        {loading ? <SpinnerCircular /> : ""}
+        <p style={{ color: "red" }}>
+          {errorFromServer ? "Error From Server" : ""}
+        </p>
+      </div>
+    </div>
   )
 }
