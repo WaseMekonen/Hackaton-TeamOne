73288c2482f366fc190c80dbadae9888ce37108b
Segedu <seggedu20@gmail.com>
2022-02-16 22:11:29 +0200

    delete bus from favorites

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,12 +1,19 @@
 const router = require("express").Router()
-const { getUser } = require("../controllers/users");
+const { getUser, insertNewLineToUserFavorites, 
+    // deleteLineFromUserFavorites 
+} = require("../controllers/users");
 
-router.get("/:localId", (req, res) => {
-    getUser(res);
+
+// localId
+router.get("/:id", (req, res) => {
+    getUser(req, res);
 });
 
-router.patch("/:id", (req, res) => {
+router.patch("/:localId", (req, res) => {
     insertNewLineToUserFavorites(req, res);
 });
 
+router.delete('/:id', (req, res) => {
+    deleteLineFromUserFavorites(req, res);
+})
 module.exports = router;
