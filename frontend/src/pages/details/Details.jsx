a46fb0656c89f6335192aa608eb7a53b7b81b6f3
MekonenWase <mekonen.wase@gmail.com>
2022-02-17 08:08:13 +0200

    add getLineDetails function

--- frontend/src/pages/details/Details.jsx
+++ frontend/src/pages/details/Details.jsx
@@ -1,16 +1,29 @@
-// import React, { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import React from "react";
-// import axios from "axios";
+import axios from "axios";
 
 import styles from "./details.module.css";
 
-export default function Details() {
-  // const [error, setError] = useState("");
+export default function Details({ line }) {
+  const [error, setError] = useState("");
+  const [lineDetail, setLineDetail] = useState("");
 
-  // console.log(lines);
-  // useEffect(() => {
-  //   getLinesDetails();
-  // }, [lines]);
+  useEffect(() => {
+    getLineDetails();
+  }, []);
+
+  const getLineDetails = () => {
+    setInterval(() => {
+      axios
+        .get(`/lines/${line}`)
+        .then((response) => {
+          setLineDetail(response.data);
+        })
+        .catch((err) => {
+          setError(err);
+        });
+    }, 5000);
+  };
 
   const isBusCrowded = (numOfPassenger) => {
     if (numOfPassenger <= 12) {
@@ -34,17 +47,19 @@ export default function Details() {
           </div>
         </section>
         <section className={styles.lineDetailsCont}>
-          {/* <h3>Line Number:{lines.busLine}</h3>
-          <h3>From {lines.start}</h3>
-          <h3>To {lines.end}</h3> */}
+          {/* <h3>Line Number:{lineDetail.busLine}</h3>
+          <h3>From {lineDetail.start}</h3>
+          <h3>To {lineDetail.end}</h3> */}
           <button className={styles.ChangeDirectionBtn}>
             Change Direction
           </button>
         </section>
         <section className={styles.stationCont}>
-          {/* <h3>Current Station: {lines.stations[lines.currentStation]}</h3> */}
+          {/* <h3>Current Station: {lineDetail.stations[lineDetail.currentStation]}</h3> */}
         </section>
-        <section className={styles.mapCont}></section>
+        <section className={styles.mapCont}>
+          <img src={lineDetail.images[line]} alt="" />
+        </section>
         <section className={styles.statusCont}>
           <div className={styles.statusEmpty}>Empty</div>
           <div className={styles.statusNormal}>Normal</div>
